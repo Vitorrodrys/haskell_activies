40e6cdc278c58f9cdd244499de5d600f8dc8f02a
Vitorrodrys <Vitorrodrys@gmail.com>
2024-08-10 23:54:38 -0300

    add p26

---
+++ P26.hs
@@ -0,0 +1,11 @@
+
+import Data.List (tails)
+
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = [ [] ]
+combinations n xs = [ y:ys | y:xs' <- tails xs
+                           , ys <- combinations (n-1) xs']
+
+main :: IO ()
+main = print (combinations 3 "abcdef")
